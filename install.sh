056b30f9efb76ae3021e2dd8bd76318db9ab7ed0
zyp2005 <1715260398@qq.com>
2020-05-30 15:21:05 +0800

    w

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+wifi-menu
+ping baidu.com 
+timedatectl set-ntp true
+fdisk /dev/sdc
+mkfs.ext4 /dev/sdc1
+mount /dev/sdc1 /mnt
+cp /run/archiso/bootmnt/mirrorlist /etc/pacman.d/
+pacstrap /mnt base linux linux-firmware base-devel
+genfstab -U /mnt >> /mnt/etc/fstab
+cp /run/archiso/bootmnt/install2.sh /mnt
+cp /run/archiso/bootmnt/pacman.conf /mnt/etc
+#cp /run/archiso/bootmnt/install2.sh /
+arch-chroot /mnt ./install2.sh
+umount /mnt /dev/sdc1
+
+
---
+++ install2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+pacman -Syu
+pacman -S vim dhcpcd dialog wpa_supplicant ntfs-3g networkmanager netctl \
+	intel-ucode grub 
+echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
+echo "zh_TW.UTF-8 UTF-8" >> /etc/locale.gen
+echo "zh_HK.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+locale-gen
+touch /etc/locale.conf
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+touch /etc/hostname
+echo "zyp-a" >> /etc/hostname
+touch /etc/hosts
+echo "127.0.0.1		localhost" >> /etc/hosts
+echo "::1		localhost" >> /etc/hosts
+echo "127.0.1.1	   	zyp-a.localdomain	zyp-a" >> /etc/hosts
+passwd
+grub-install --target=i386-pc /dev/sdc
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl start NetworkManger
+systemctl enable NetworkManger
+nmtui
+
+
