1dbf5cfd17b46c3878930e41508416045cf73134
zyp2005 <1715260398@qq.com>
2020-05-30 19:43:11 +0800

    1

--- install4.sh
+++ install4.sh
@@ -1,6 +1,7 @@
 sudo pacman -S xf86-video-intel xorg xorg-xinit wqy-microhei i3 feh compton \
 	polybar ttf-font-awesome sakura clang fcitx-im fcitx-configtool \
-	chromium fcitx-googlepinyin rofi curl neovim alsa-utils alsa-tools
+	chromium fcitx-googlepinyin rofi curl neovim alsa-utils alsa-tools \
+	nodejs npm
 mkdir .config
 mkdir .config/i3 .config/polybar .config/sakura
 cp vim/.xinitrc .xinitrc
