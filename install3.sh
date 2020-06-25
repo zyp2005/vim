c6ae5179b77db76c69e274494f65b411832d9d3f
zyp2005 <59333102+zyp2005@users.noreply.github.com>
2020-06-25 16:53:44 +0800

    Update install3.sh

--- install3.sh
+++ install3.sh
@@ -1,8 +1,8 @@
 useradd -m -G wheel zyp
 passwd zyp
-pacman -S sudo 
+#pacman -S sudo 
 echo " %wheel ALL=(ALL)ALL" >> /etc/sudoers
 cd /home/zyp
 su zyp
-git clone https://github.com/zyp2005/vim
+#git clone https://github.com/zyp2005/vim
 reboot
