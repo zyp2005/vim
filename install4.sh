78fad22e9180e3aaa1ceb9fa5862fa271aeb9322
zyp2005 <1715260398@qq.com>
2020-08-07 19:53:17 +0800

    ss

--- install4.sh
+++ install4.sh
@@ -1,6 +1,6 @@
 sudo pacman -S xf86-video-intel xorg xorg-xinit wqy-microhei i3 feh compton \
-	polybar ttf-font-awesome sakura clang fcitx-im fcitx-configtool \
-	chromium fcitx-googlepinyin rofi curl  alsa alsa-utils \
+	polybar ttf-font-awesome sakura clang  \
+	chromium  rofi curl  alsa alsa-utils \
 	nodejs npm fzf yay ranger
 mkdir .config
 mkdir .config/i3 .config/polybar .config/sakura
@@ -12,23 +12,24 @@ cp vim/config/sakura/sakura.conf  .config/sakura/
 chmod +x ~/.config/polybar/launch.sh
 echo "export LANG=zh_CN.UTF-8" >> .bashrc
 echo "export LC_ALL=zh_CN.UTF-8" >> .bashrc
+echo "export INPUT_METHOD=\"fcitx\"" >> .bashrc
 echo "export GTK_IM_MODULE=\"fcitx\"" >> .bashrc
 echo "export QT_IM_MODULE=\"fcitx\"" >> .bashrc
 echo "export XMODIFIERS=\"@im=fcitx\"" >> .bashrc
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-cp vim/vimrc .vim/
+echo "alias w3m='w3m -o auto_image=FLASE -o ext_image_viewer=1'" >> .bashrc
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#cp vim/vimrc .vim/
 mkdir picture
 cp vim/20120806133222_2R2eN.jpeg picture
-sudo pacman -S alsa-firmware alsa-tools pulseaudio-alsa
-yay -S lf powerline-fonts-git
-yay -S mate-media
-yay -S wps-office-cn cups  ttf-wps-fonts wps-office-mime-cn wps-office-mui-zh-cn
-yay -S adobe-source-code-pro-fonts adobe-source-han-mono-cn-fonts
-adobe-source-han-sans-cn-fonts adobe-source-han-sans-tw-fonts adobe-source-serif-pro-fonts 
-cmake ctags wget elinks gdb go lazygit ccls lua mate-volume-control neofetch 
-neovim-nightly neovim-remote nerd-fonts-source-code-pro npm
-ruby w3m you-get yarn 
+yay -S alsa-firmware alsa-tools pulseaudio-alsa
+	lf powerline-fonts-git mate-media \
+	wps-office-cn cups  ttf-wps-fonts wps-office-mime-cn wps-office-mui-zh-cn \
+	adobe-source-code-pro-fonts adobe-source-han-mono-cn-fonts \
+	adobe-source-han-sans-cn-fonts  adobe-source-serif-pro-fonts \
+	cmake ctags wget elinks gdb go lazygit ccls lua mate-volume-control neofetch \
+	neovim-nightly neovim-remote nerd-fonts-source-code-pro npm \
+	ruby w3m you-get yarn cargo qq-linux
 echo "done"
 
 
